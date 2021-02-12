9fca9770b5cb0c0a1b0e5ef1fc57aee024cac470
Dan Krasilnikov <krasilneg@yandex.ru>
2021-02-12 15:06:46 +1000

    v2.0.0

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  aclMetaMap: require('./access/aclMetaMap'),
+  aclmongo: require('./access/aclmongo'),
+  amAccessManager: require('./access/amAccessManager'),
+  DsAcl: require('./access/DsAcl'),
+  DsRoleAccessChangeLogger: require('./access/DsRoleAccessChangeLogger'),
+  DsRoleAccessManager: require('./access/DsRoleAccessManager')
+};
